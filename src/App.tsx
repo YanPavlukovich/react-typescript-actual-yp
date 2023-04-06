ba75ebdaa0f85a2a5c3a6c4b5e32d5b7ea972e88
YanPavlukovich <filin022@gmail.com>
2023-04-06 23:12:21 +0300

    декомпозиция users and todos, установка router для навигации

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,25 @@
-import { useEffect, useState } from "react";
-import Card, { CardVariant } from "./components/Card";
-import { ITodo, IUser } from "./types/types";
-import axios from "axios";
-import List from "./components/List";
-import UserItem from "./components/UserItem";
-import TodoItem from "./components/TodoItem";
-import EventsExample from "./components/EventsExample";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import UsersPage from "./components/UsersPage";
+import TodosPage from "./components/TodosPage";
+import UserItemPage from "./components/UserItemPage";
+import TodoItemPage from "./components/TodoItemPage";
 
 const App = () => {
-  const [users, setUsers] = useState<IUser[]>([]);
-  const [todos, setTodos] = useState<ITodo[]>([]);
-
-  useEffect(() => {
-    fetchUsers();
-    fetchTodos();
-  }, []);
-
-  async function fetchUsers() {
-    try {
-      const response = await axios.get<IUser[]>(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      setUsers(response.data);
-    } catch (e) {
-      alert(e);
-    }
-  }
-  async function fetchTodos() {
-    try {
-      const response = await axios.get<ITodo[]>(
-        "https://jsonplaceholder.typicode.com/todos?_limit=10"
-      );
-      setTodos(response.data);
-    } catch (e) {
-      alert(e);
-    }
-  }
-
   return (
-    <div>
-      <EventsExample />
-      <Card variant={CardVariant.outlined} width="200px" height="200px">
-        <button>Button</button>
-      </Card>
-      <List
-        items={users}
-        renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
-      />
-      <List
-        items={todos}
-        renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
-      />
-    </div>
+    <BrowserRouter>
+      <div>
+        <div>
+          <NavLink to={"/users"}>Users</NavLink>
+          <NavLink to={"/todos"}>Todos List</NavLink>
+        </div>
+        <Routes>
+          <Route path={"/users"} element={<UsersPage />} />
+          <Route path={"/todos"} element={<TodosPage />} />
+          <Route path={"/users/:id"} element={<UserItemPage />} />
+          <Route path={"/todos/:id"} element={<TodoItemPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/UserItemPage.tsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { FC, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IUser } from "../types/types";
+
+interface UserItemPageParams {
+  [id: string]: string;
+}
+
+const UserItemPage: FC = () => {
+  const [user, setUser] = useState<IUser | null>(null);
+  const params = useParams<UserItemPageParams>();
+
+  useEffect(() => {
+    fetchUser();
+  });
+
+  async function fetchUser() {
+    try {
+      const response = await axios.get<IUser>(
+        "https://jsonplaceholder.typicode.com/users/" + params.id
+      );
+      setUser(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
+  return (
+    <div>
+      <button>Back</button>
+      <h1>{user?.name} user page</h1>
+      <div>{user?.email}</div>
+      <div>
+        {user?.address.city} {user?.address.city} {user?.address.zipcode}
+      </div>
+    </div>
+  );
+};
+
+export default UserItemPage;
